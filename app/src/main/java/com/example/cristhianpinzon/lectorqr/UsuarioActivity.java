e2d2ca199178cfb8dca9a19356430b4bff7a8a7a
ivan <ivan.espitia@uptc.edu.co>
2017-08-11 10:53:31 -0500

    peticion de permisos

--- app/src/main/java/com/example/cristhianpinzon/lectorqr/UsuarioActivity.java
+++ app/src/main/java/com/example/cristhianpinzon/lectorqr/UsuarioActivity.java
@@ -1,6 +1,8 @@
 package com.example.cristhianpinzon.lectorqr;
 
+import android.Manifest;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,10 +10,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.example.cristhianpinzon.lectorqr.Persistence.logic.DB.DatabaseAccess;
 import com.example.cristhianpinzon.lectorqr.Persistence.logic.User;
 import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
@@ -43,8 +52,12 @@ public class UsuarioActivity extends AppCompatActivity implements ZXingScannerVi
             cargarLoginPrincipal();
         }
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            permission_request();
+        }else {
+            QrScanner(null);
+        }
         _btnScaner = (Button) findViewById(R.id.btn_scan);
-
     }
 
     private void cargarLoginPrincipal() {
@@ -92,22 +105,52 @@ public class UsuarioActivity extends AppCompatActivity implements ZXingScannerVi
 
     @Override
     public void handleResult(Result rawResult) {
-
         Log.e("Resultado - > ", rawResult.getText()); // Prints scan results<br />
         Log.e("Tipo de escaneado -> ", rawResult.getBarcodeFormat().toString());
+    }
 
+    private void permission_request(){
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(permissionListener())
+                .withErrorListener(errorListener())
+                .check();
+    }
 
+    private PermissionListener permissionListener(){
+        return new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse response) {
+                Toast.makeText(UsuarioActivity.this, "Permisos obtenidos", Toast.LENGTH_SHORT).show();
+                QrScanner(null);
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse response) {
+                Toast.makeText(UsuarioActivity.this, "Permisos negados", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                token.continuePermissionRequest();
+            }
+        };
+    }
+
+    private PermissionRequestErrorListener errorListener(){
+        return new PermissionRequestErrorListener() {
+            @Override
+            public void onError(DexterError error) {
+                Toast.makeText(UsuarioActivity.this, "Ocurrio un error en la peticion de permisos", Toast.LENGTH_SHORT).show();
+            }
+        };
     }
 
     public void QrScanner(View view){
-
-
         mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view
         setContentView(mScannerView);
-
         mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
         mScannerView.startCamera();         // Start camera
-
     }
 
     @Override
